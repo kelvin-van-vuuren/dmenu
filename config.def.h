8ad04d1422100ca2d07c4c25183026b94f848596
Kelvin van Vuuren <kelvin@kelvinvv.xyz>
2022-12-09 13:41:58 +0000

    Update border width and alpha

--- config.def.h
+++ config.def.h
@@ -3,7 +3,7 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
-static const unsigned int alpha = 0xe0;
+static const unsigned int alpha = 0xF0;
 static int centered = 0;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
@@ -50,7 +50,7 @@ static unsigned int lines      = 0;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static const unsigned int border_width = 5;
+static const unsigned int border_width = 2;
 
 
 /*
